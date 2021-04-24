bc652193f8d783de1161c96a165c787610cde72a
ayo <hakeemanibaba@yahoo.com>
2021-04-24 23:40:25 +0100

    initial commmit

--- config/settings.py
+++ config/settings.py
@@ -14,9 +14,7 @@ import os
 from pathlib import Path
 
 
-#initializing environment variables
-env = environ.Env() 
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
