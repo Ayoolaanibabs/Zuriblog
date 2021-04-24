72f76903ba658973d1dcd761a48508053c778c44
ayo <hakeemanibaba@yahoo.com>
2021-04-24 16:55:23 +0100

    changes for heroku

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-import environ
+import django-environ
 
 #initializing environment variables
 env = environ.Env()
